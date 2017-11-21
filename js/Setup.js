3325317be11db006a54e10e8c025e4fd4d1859f1
zengmin shi <shizm@yonyou.com>
2017-11-21 16:20:35 +0800

    commit

---
+++ js/Setup.js
@@ -0,0 +1,30 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+
+export default class Setup extends Component {
+  render() {
+    return (
+      <View >
+        <Text>
+          Text1
+        </Text>
+        <Text >
+          Text2
+        </Text>
+        <Text >
+          Text3
+        </Text>
+      </View>
+    );
+  }
+}
